dbdfffa396054529678993dc2579f0bda1bb13f9
Steve Fan <19037626d@connect.polyu.hk>
2020-02-27 09:12:52 +0800

    upgrade to core 3.1

--- src/CromulentBisgetti.DemoApp/Startup.cs
+++ src/CromulentBisgetti.DemoApp/Startup.cs
@@ -9,54 +9,49 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+using Microsoft.Extensions.Hosting;
 
-namespace CromulentBisgetti.DemoApp
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
-                .AddJsonOptions(options =>
-                {
-                    options.SerializerSettings.ContractResolver = null;
-                });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-
-            app.UseMvcWithDefaultRoute();
-        }
+namespace CromulentBisgetti.DemoApp {
+  public class Startup {
+    public Startup(IConfiguration configuration) {
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services) {
+
+
+      services.Configure<CookiePolicyOptions>(options => {
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+      });
+
+      services
+        .AddMvc(option => { option.EnableEndpointRouting = false; })
+        .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+          .AddNewtonsoftJson(options => {
+            options.SerializerSettings.ContractResolver = null;
+          });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+      if (env.IsDevelopment()) {
+        app.UseDeveloperExceptionPage();
+      } else {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseCookiePolicy();
+
+      app.UseMvcWithDefaultRoute();
+    }
+  }
 }
